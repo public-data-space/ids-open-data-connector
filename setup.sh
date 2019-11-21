d1cbca4cb63c8851acc76c5a45506f53633613b4
Vincent Bohlen <vincent.bohlen@fokus.fraunhofer.de>
2019-11-21 13:14:52 +0100

    Use public github urls for clone

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #Clone adapters
-git clone git@gitlab.fokus.fraunhofer.de:ids/odc-adapter-postgres.git ./adapters/odc-adapter-postgres
-git clone git@gitlab.fokus.fraunhofer.de:ids/odc-adapter-ckan.git ./adapters/odc-adapter-ckan
+git clone https://github.com/public-data-space/odc-adapter-postgres.git ./adapters/odc-adapter-postgres
+git clone https://github.com/public-data-space/odc-adapter-ckan.git ./adapters/odc-adapter-ckan
 
 #Clone backend
-git clone git@gitlab.fokus.fraunhofer.de:ids/odc-config-manager.git ./backend/odc-config-manager
-git clone git@gitlab.fokus.fraunhofer.de:ids/odc-adapter-gateway.git ./backend/odc-adapter-gateway
-git clone git@gitlab.fokus.fraunhofer.de:ids/odc-manager.git ./backend/odc-manager
+git clone https://github.com/public-data-space/odc-config-manager.git ./backend/odc-config-manager
+git clone https://github.com/public-data-space/odc-adapter-gateway.git ./backend/odc-adapter-gateway
+git clone https://github.com/public-data-space/odc-manager.git ./backend/odc-manager
 
 #Clone frontend
-git clone git@gitlab.fokus.fraunhofer.de:ids/odc-frontend.git ./frontend/odc-frontend
+git clone https://github.com/public-data-space/odc-frontend.git ./frontend/odc-frontend
 
 ./startup.sh
